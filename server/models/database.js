03262164d57097d6d623c73247ebf0dc12306469
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-12-03 14:01:11 -0800

    adds PUT route to update profiles

--- server/models/database.js
+++ server/models/database.js
@@ -5,7 +5,18 @@ async function connect() {
     const client = new pg.Client(CONFIG)
     await client.connect()
     const query = await client.query(
-        'CREATE TABLE profiles(id SERIAL PRIMARY KEY, username VARCHAR(16) not null, password VARCHAR(32) not null, email VARCHAR(32) not null);')
+        `CREATE TABLE profiles(
+            username VARCHAR(16) PRIMARY KEY,
+            password VARCHAR(32) NOT NULL,
+            email VARCHAR(32) NOT NULL,
+            first_name VARCHAR(32),
+            last_name VARCHAR(32),
+            telephone VARCHAR(16),
+            street_address TEXT,
+            city VARCHAR(32),
+            state VARCHAR(16),
+            zip_code CHAR(5)
+        )`)
     await client.end()
 }
 
--- server/routes.js
+++ server/routes.js
@@ -7,6 +7,17 @@ function createHash(password){
     return crypto.createHash('md5').update(password).digest('hex')
 }
 
+const UPDATE_FIELDS = new Set([
+    "email",
+    "first_name",
+    "last_name",
+    "telephone",
+    "street_address",
+    "city",
+    "state",
+    "zip_code",
+])
+
 routes.post('/profiles', (req, res) => {
     let {username, password, email} = req.body.fields
     password = createHash(password)
@@ -30,7 +41,7 @@ routes.post('/profiles', (req, res) => {
         const results = []
         try{
             //obtain list of all profiles for response
-            const query = await client.query('SELECT username, email FROM profiles ORDER BY id ASC')
+            const query = await client.query('SELECT username FROM profiles ORDER BY username ASC')
             for (row of query.rows) {
                 results.push(row)
             }
@@ -42,5 +53,50 @@ routes.post('/profiles', (req, res) => {
     })
     pool.end()
 });
+
+routes.put('/profiles/:username', (req, res) => {
+    //obtain a list of all valid fields in the body
+    const fields = Object.keys(req.body.fields).filter(key => UPDATE_FIELDS.has(key))
+    //obtain the data from these valid fields
+    let profileData = {}
+    for (field of fields) {
+        profileData[field] = req.body.fields[field]
+    }
+
+    const username = req.params.username
+
+    let pool = new pg.Pool(CONFIG)
+    pool.connect(async (error, client, done) => {
+        if (error) {
+            done()
+            console.log(error)
+            return res.status(500).json(error)
+        }
+        try{
+            //query snippet for updating profile fields
+            const fieldSetter = fields.map((field, index) => `${field}=$${index + 1}`).join(', ')
+            //an array of values in the same order as in the query snippet
+            const fieldValues = fields.map(field => profileData[field])
+            fieldValues.push(username)
+            //update profile
+            const query = `UPDATE profiles SET ${fieldSetter} WHERE username=$${fields.length + 1}`
+            console.log(query)
+            await client.query(query, fieldValues)
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+        let updated;
+        try{
+            //obtain updated profile for response
+            const query = await client.query('SELECT * FROM profiles WHERE username=$1', [username])
+            updated = query.rows[0]
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+        done()
+        res.json(updated)
+    })
+    pool.end()
+})
 
 module.exports =  routes
