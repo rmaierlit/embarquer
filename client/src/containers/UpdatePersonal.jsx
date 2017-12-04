64b80cc40fce3b67fcb54d8050936cac1e2d2074
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-12-03 18:55:52 -0800

    improves title

--- client/src/containers/UpdatePersonal.jsx
+++ client/src/containers/UpdatePersonal.jsx
@@ -4,6 +4,6 @@ import {checkInfo} from '../actions'
 const FIELD_NAMES = ['first_name', 'last_name', 'telephone']
 const NEXT_PATH = '/onboard/step3'
 const COMPLETE_FLAG = 'personalComplete'
-const TITLE = 'Personal Information'
+const TITLE = 'Update Personal Information'
 
 export default InfoContainer(TITLE, FIELD_NAMES, NEXT_PATH, COMPLETE_FLAG, checkInfo)
